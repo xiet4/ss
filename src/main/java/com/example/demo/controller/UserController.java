f5fe1e6ca3b5b19d14b8c5121996a393e4d6d069
xiet4 <50798584+xiet4@users.noreply.github.com>
2024-01-11 15:02:25 +0800

    Add files via upload

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.Usermapper;
 import com.example.demo.service.userService;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -66,4 +78,44 @@ public class UserController {
         return userService.page(page,queryWrapper);
 
     }
+
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws IOException {
+           List<User> list = userService.list();
+        ExcelWriter excelWriter = ExcelUtil.getWriter(true);
+
+        excelWriter.write(list,true);
+
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        response.setHeader("Content-Disposition","attachment;filename=test.xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        excelWriter.flush(out, true);
+        excelWriter.close();
+        out.close();
+    }
+
+    @PostMapping("/import")
+    public void imp(MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+
+         List<List<Object>> list = reader.read(1);
+        List<User> users = CollUtil.newArrayList();
+        for(List<Object> row:list ){
+         User user=new User();
+         user.setUsername(row.get(1).toString());
+         user.setPassword(row.get(2).toString());
+         user.setNickname(row.get(3).toString());
+         user.setEmail(row.get(4).toString());
+            user.setPhone(row.get(5).toString());
+            user.setAddress(row.get(6).toString());
+            users.add(user);
+         }
+
+        userService.saveBatch(users);
+        System.out.println(list);
+
+    }
+
 }
